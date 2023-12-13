a11769636be2414f5171e25193cf4126b7774fca
Praful Chauhan <cprafulm@gmail.com>
2023-12-13 22:13:00 +0530

    Login with Google related changes

--- app/(others)/login/page.jsx
+++ app/(others)/login/page.jsx
@@ -34,11 +34,14 @@ const LogIn = () => {
                 <div className="row y-gap-20 pt-30">
                   <div className="col-12">
                     <div className="text-center">or sign in with</div>
-                  </div>
-    <GoogleOAuthProvider 
+                    
+                    <div className="text-center"><GoogleOAuthProvider 
       clientId="387946406507-3akrm9q830gnja0pdspabuemif5fnd1e.apps.googleusercontent.com">
                   <LoginWithSocial />
-</GoogleOAuthProvider>
+</GoogleOAuthProvider></div>
+                    
+                  </div>
+    
                   <div className="col-12">
                     <div className="text-center px-30">
                       By creating an account, you agree to our Terms of Service
--- components/common/LoginWithSocial.jsx
+++ components/common/LoginWithSocial.jsx
@@ -65,19 +65,19 @@ const LoginWithSocial = () => {
 
   return (
     <>
-      <div className="col-md-6 col-12">
-        <button className="button col-12 -outline-blue-1 text-blue-1 py-15 rounded-8 ">
+      <div className="col-md-12 col-12">
+        {/* <button className="button col-12 -outline-blue-1 text-blue-1 py-15 rounded-8 ">
           <i className="icon-apple text-15 mr-10" />
           Facebook
-        </button>
+        </button> */}
       </div>
 
-      <div className="col-md-6 col-12">
-        <button className="button col-12 -outline-red-1 text-red-1 py-15 rounded-8 "
+      <div className="col-md-12 col-12">
+        {/* <button className="button col-12 -outline-red-1 text-red-1 py-15 rounded-8 "
         onClick={(e) => handleGoogleLoginSuccess1(e)}>
           <i className="icon-apple text-15 mr-10" />
           Google
-        </button>
+        </button> */}
         {/* <GoogleLogin
       clientId="387946406507-3akrm9q830gnja0pdspabuemif5fnd1e.apps.googleusercontent.com"
       buttonText="Login with Google"
@@ -90,7 +90,7 @@ const LoginWithSocial = () => {
   onError={() => {
     console.log('Login Failed');
   }}
-/>;
+/>
         {/* <GoogleOAuthProvider clientId="387946406507-3akrm9q830gnja0pdspabuemif5fnd1e.apps.googleusercontent.com">
           
         <GoogleLogin
