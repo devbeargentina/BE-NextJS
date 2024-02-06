2020575df63efb6720c117185d99fc8b5dd6f038
devbeargentina <149800336+devbeargentina@users.noreply.github.com>
2024-02-06 11:45:13 +0530

    Update Overview.jsx

--- components/hotel-single/Overview.jsx
+++ components/hotel-single/Overview.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import $ from 'jquery'; // Import jQuery
+// import $ from 'jquery'; // Import jQuery
 
 const Overview = ({ hotel }) => {
   const [showFullContent, setShowFullContent] = useState(false);
@@ -9,27 +9,27 @@ const Overview = ({ hotel }) => {
     shortdesc = hotel.descriptions.substring(0, 200) + "...";
   }
 
-  const toggleContent = () => {
-    if (showFullContent) {
-      $(".overview").html(shortdesc);
-      $(".showmore").html("Show More");
-    } else {
-      $(".overview").html(hotel.descriptions);
-      $(".showmore").html("Show Less");
-    }
-    setShowFullContent(!showFullContent);
-  };
+  // const toggleContent = () => {
+  //   if (showFullContent) {
+  //     $(".overview").html(shortdesc);
+  //     $(".showmore").html("Show More");
+  //   } else {
+  //     $(".overview").html(hotel.descriptions);
+  //     $(".showmore").html("Show Less");
+  //   }
+  //   setShowFullContent(!showFullContent);
+  // };
 
   return (
     <>
       <h3 className="text-22 fw-500 pt-40 border-top-light">Overview</h3>
       <p className="text-dark-1 text-15 mt-20 overview">{shortdesc}</p>
-      <a
+      {/* <a
         onClick={toggleContent}
         className="d-block text-14 text-blue-1 fw-500 underline mt-10 showmore"
       >
         {showFullContent ? 'Show Less' : 'Show More'}
-      </a>
+      </a> */}
     </>
   );
 };
